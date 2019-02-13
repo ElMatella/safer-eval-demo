9a0b644f8217fc5b6c5b4a7ba2ac3c604bb7e9ea
commenthol <commenthol@users.noreply.github.com>
2019-02-13 18:22:29 +0100

    fix: correct used package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { h, render } from 'preact'
-import safeEval from 'safe-eval'
+import saferEval from 'safer-eval'
 
 render((
-  <h1>1 + 1 = {safeEval('1 + 1')}</h1>
+  <h1>1 + 1 = {saferEval('1 + 1')}</h1>
 ), document.getElementById('content'))
